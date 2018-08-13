2a407b5b6f1bdeef22954303add0669abea06761
honchwong <honchwong404@gmail.com>
2018-08-13 19:09:58 +0800

    创建HCExpandableLabel文件

---
+++ HCExpandableLabel/HCExpandableLabel/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCExpandableLabel
+//
+//  Created by 黄鸿昌 on 2018/8/13.
+//  Copyright © 2018年 黄鸿昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HCExpandableLabel/HCExpandableLabel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HCExpandableLabel
+//
+//  Created by 黄鸿昌 on 2018/8/13.
+//  Copyright © 2018年 黄鸿昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
