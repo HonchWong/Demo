c10226af9be298a2dfcc8a15b22787ee4514a7dc
honchwong <honchwong404@gmail.com>
2018-08-20 19:59:49 +0800

    add HCLog

---
+++ HCLog/HCLog/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HCLog
+//
+//  Created by 黄鸿昌 on 2018/8/20.
+//  Copyright © 2018年 黄鸿昌. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
