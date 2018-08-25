ea9cf737c46d4049df177e9940a8759c275371a9
honchwong <honchwong404@gmail.com>
2018-08-26 00:59:54 +0800

    上传文件到个人网盘

---
+++ HCLog/HCLog/HCLog/HCLogUploadManager.h
@@ -0,0 +1,15 @@
+//
+//  HCLogUploadManager.h
+//  HCLog
+//
+//  Created by 黄鸿昌 on 2018/8/26.
+//  Copyright © 2018年 黄鸿昌. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HCLogUploadManager : NSObject
+
++ (void)upload;
+
+@end
